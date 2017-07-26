ac68c5baaa6a03274d6e363124e847fd0fd77865
kinjoyuji <kinjoyuji@gmail.com>
2017-07-27 01:11:22 +0900

    homeViewのoutlet接続

---
+++ UdemyInstagramClone/UdemyInstagramClone/HeaderCollectionReusableView.swift
@@ -0,0 +1,31 @@
+//
+//  HeaderCollectionReusableView.swift
+//  UdemyInstagramClone
+//
+//  Created by 金城裕治 on 2017/07/27.
+//  Copyright © 2017年 Yuji. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+  
+  
+  @IBOutlet weak var avaImg: UIImageView!
+  @IBOutlet weak var fullname: UILabel!
+  @IBOutlet weak var webTxt: UITextView!
+  @IBOutlet weak var bioLbl: UILabel!
+  
+  @IBOutlet weak var posts: UILabel!
+  @IBOutlet weak var followers: UILabel!
+  @IBOutlet weak var followings: UILabel!
+  @IBOutlet weak var postTitle: UILabel!
+  @IBOutlet weak var followerTitle: UILabel!
+  @IBOutlet weak var followingTitle: UILabel!
+  
+  @IBOutlet weak var button: UIButton!
+  
+  
+  
+  
+}
---
+++ UdemyInstagramClone/UdemyInstagramClone/HomeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HomeCollectionViewCell.swift
+//  UdemyInstagramClone
+//
+//  Created by 金城裕治 on 2017/07/27.
+//  Copyright © 2017年 Yuji. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var picImg: UIImageView!
+  
+}
